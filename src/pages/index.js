4a2845e4b238e90f4e7e3ffcccfaa084f26e1b3d
Raul Jimenez Ortega <rjimenez@esri.com>
2023-05-08 11:25:48 +0200

    Testing changes in landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+require('@site/static/img/undraw_docusaurus_react.svg').default
+import HhkaosImageUrl from '@site/static/img/hhkaos-raul-jimenez-ortega.jpeg';
+
 
 import styles from './index.module.css';
 
@@ -13,12 +16,13 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+        <img className="rounded img-medium" src={HhkaosImageUrl} alt="Raul Jimenez photography"/>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Go to my digital brain 🧠
           </Link>
         </div>
       </div>
@@ -30,8 +34,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="This website contain my digital brain">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
